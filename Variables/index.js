bfce842f8fae0fe1cf4b82feadba7a825e19953e
Lucas <lucass20586@gmail.com>
2021-01-18 16:04:36 -0300

    Variables

---
+++ Variables/index.js
@@ -0,0 +1,11 @@
+// variables = caixas que você armaenas suas coisas
+
+let name = 'Lucas';
+console.log(name);
+
+// Não coloque um número ou uma letra apenas (1name)
+// Não podem conter espaços ou "-"
+// Elas devem começar com letra minúscual
+
+// pode declarar duas ao mesmo tempor
+let firtName, lastName;
